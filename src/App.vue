69b70f2a3f1fc7f3370c4ec512af9b0da70939d8
Marchandise Rudy <r.marchandise@sfr.fr>
2017-08-07 15:18:45 +0200

    Password Component

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <password></password>
+  </div>
+</template>
+
+<script>
+import Password from './components/Password'
+
+export default {
+  name: 'app',
+  components: {
+    Password
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Password.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="password">
+    <div class="ui left corner labeled input">
+
+      <input v-if="showPassword" type="text" v-model="password" placeholder="Mot de passe">
+      <input v-if="!showPassword" type="password" v-model="password" placeholder="Mot de passe">
+
+      <div class="ui left corner label">
+        <i class="asterisk icon"></i>
+      </div>
+
+    </div>
+
+    <button v-on:click="showPassword = !showPassword" class="ui icon button">
+      <i :class="inputButtonIcon + ' icon'"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'password',
+  data () {
+    return {
+      password: '',
+      showPassword: false
+    }
+  },
+  computed: {
+    inputButtonIcon: function () {
+      return this.showPassword ? 'hide' : 'unhide'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
